984122020ffe7d4219e867c8ed73c44d81e187fc
DaeChul00 <103113850+DaeChul00@users.noreply.github.com>
2022-09-04 19:17:03 +0900

    Add files via upload

---
+++ day7/main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+// baekjoon 10801 카드게임
+using namespace std;
+
+int main() {
+
+    int a[10];
+    int b[10];
+
+    int aw = 0, bw = 0;
+
+    for (int i = 0; i < 10; i++) {
+        cin >> a[i];
+    }
+
+    for (int i = 0; i < 10; i++) {
+        cin >> b[i];
+    }
+
+    for (int i = 0; i < 10; i++) {
+        if (a[i] > b[i]) {
+            aw++;
+        }
+        else if (b[i] > a[i]) {
+            bw++;
+        }
+
+    }
+    if (aw > bw) cout << "A";
+    else if (bw > aw) cout << "B";
+    else cout << "D";
+
+}
