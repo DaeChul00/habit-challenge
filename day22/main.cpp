a17a6bcba73d421c179f88d6877542316ad9e86c
DaeChul00 <103113850+DaeChul00@users.noreply.github.com>
2022-09-19 19:34:46 +0900

    Add files via upload

---
+++ day22/main.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+using namespace std;
+#define endl '\n'
+
+int main() {
+    int x;
+    cin >> x;
+
+    int zigzag = 0, sum = 0;
+    while (x > sum)
+    {
+        zigzag++;
+        sum += zigzag;
+
+    }
+
+    int numerator, denominator;
+    if (zigzag % 2 == 0)
+    {
+        denominator = sum - x + 1;
+        numerator = zigzag - denominator + 1;
+    }
+    else
+    {
+        numerator = sum - x + 1;
+        denominator = zigzag - numerator + 1;
+    }
+
+    cout << numerator << '/' << denominator << endl;
+
+    return 0;
+}
